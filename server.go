e260a19ce40b3689e9ee0c8eac3e8a814ff81bf4
Jesse <jessexinyu@foxmail.com>
2023-10-09 17:01:20 +0800

    init

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+
+	return server
+}
+
+func (this *Server) Start() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Listener accept err:", err)
+			continue
+		}
+
+	}
+}
