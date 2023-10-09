6fee8104f7cd08e6675b93cf98396f592105b8fc
Jesse <jesse@example.com>
2023-10-09 21:07:20 +0800

    feat: handler function

--- server.go
+++ server.go
@@ -19,6 +19,12 @@ func NewServer(ip string, port int) *Server {
 	return server
 }
 
+// 处理业务
+func (this *Server) Handler(conn net.Conn) {
+	fmt.Println("用户建立链接")
+}
+
+// 启动服务
 func (this *Server) Start() {
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
@@ -26,13 +32,16 @@ func (this *Server) Start() {
 		return
 	}
 	defer listener.Close()
+	fmt.Printf("服务已启动：%s:%d", this.Ip, this.Port)
 
 	for {
+		// 等待并返回一个已连接的侦听器
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Listener accept err:", err)
 			continue
 		}
 
+		go this.Handler(conn)
 	}
 }
